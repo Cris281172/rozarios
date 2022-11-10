0bea9328d046b7321d959887d1fc6756dfe96a36
Cris281172 <krzysiu.juczynski@gmail.com>
2022-11-10 14:49:04 +0100

    WIP

--- index.js
+++ index.js
@@ -21,6 +21,6 @@ app.get('/lokalizacja', (req, res) => {
     res.render('localization')
 })
 
-app.set("port", 3000);
-
-app.listen('3000');
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
